626b7b587de9639a95803de8b183a1917f5adf0b
Nick Cooke <nj.cooke@outlook.com>
2024-05-19 19:54:42 +0100

    minor formatting

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,21 +1,22 @@
 {-# LANGUAGE ImplicitParams    #-}
 {-# LANGUAGE OverloadedLabels  #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module MyLib where
 
 import           BasicSettings
 import           Buttons
-import           Data.IORef      (newIORef)
-import           Data.Text       (pack)
-import qualified GI.Gdk          as Gdk
+import           Data.IORef    (newIORef)
+import           Data.Text     (pack)
+import qualified GI.Gdk        as Gdk
 import           GI.Gio
-import qualified GI.Gtk          as Gtk
+import qualified GI.Gtk        as Gtk
 import           Menu
 
 resetNotebookLabels :: Gtk.Notebook -> IO ()
 resetNotebookLabels notebook = do
   nPages <- Gtk.notebookGetNPages notebook
-  mapM_ (updatePageLabel notebook) [0..fromIntegral nPages - 1]
+  mapM_ (updatePageLabel notebook) [0 .. fromIntegral nPages - 1]
 
 updatePageLabel :: Gtk.Notebook -> Int -> IO ()
 updatePageLabel notebook pageIndex = do
@@ -27,43 +28,46 @@ updatePageLabel notebook pageIndex = do
       label <- Gtk.labelNew (Just newLabel)
       Gtk.notebookSetTabLabel notebook pageWidget (Just label)
 
-data NewPage = Append | InsertBefore | InsertAfter deriving Eq
+data NewPage = Append | InsertBefore | InsertAfter deriving (Eq)
 
 addPage :: NewPage -> Gtk.Notebook -> Gtk.Application -> IO ()
 addPage addOrInsert notebook app = do
-      surfaceRef <- newIORef Nothing
-      lastPosRef <- newIORef Nothing
-      isDrawingRef <- newIORef False
-      penColorRef <- newIORef White
-      pg <- Gtk.notebookGetNPages notebook
-      pageLabel <- new Gtk.Label [ #label := pack $ "Page " ++ show (pg + 1)]
-      currentPosition <- Gtk.notebookGetCurrentPage notebook
-      menu <- rightClickMenu isDrawingRef penColorRef app
-      drawingArea <- new Gtk.DrawingArea
-        [ #widthRequest := pageWidth
-        , #heightRequest := pageHeight
-        , On #draw $ handleDraw surfaceRef ?self
-        , On #buttonPressEvent $ \event
-          -> buttonPress event isDrawingRef
-        , On #buttonReleaseEvent $ \event
-          -> buttonRelease event isDrawingRef lastPosRef
-        , On #motionNotifyEvent $ \event
-          -> motionNotify event surfaceRef lastPosRef isDrawingRef penColorRef ?self
-        , On #buttonPressEvent $ \event
-          -> rightClickNotify event menu
-        , On #realize $ realize surfaceRef
-        ]
-      _ <- case addOrInsert of
-        Append -> Gtk.notebookAppendPage notebook drawingArea (Just pageLabel)
-        InsertAfter -> Gtk.notebookInsertPage notebook drawingArea (Just pageLabel) (currentPosition + 1)
-        InsertBefore -> Gtk.notebookInsertPage notebook drawingArea (Just pageLabel) currentPosition
-      resetNotebookLabels notebook
-      #addEvents drawingArea
-        [ Gdk.EventMaskButtonPressMask
-        , Gdk.EventMaskPointerMotionMask
-        , Gdk.EventMaskButtonReleaseMask
-        ]
-      #showAll notebook
+  surfaceRef <- newIORef Nothing
+  lastPosRef <- newIORef Nothing
+  isDrawingRef <- newIORef False
+  penColorRef <- newIORef White
+  pg <- Gtk.notebookGetNPages notebook
+  pageLabel <- new Gtk.Label [#label := pack $ "Page " ++ show (pg + 1)]
+  currentPosition <- Gtk.notebookGetCurrentPage notebook
+  menu <- rightClickMenu isDrawingRef penColorRef app
+  drawingArea <-
+    new
+      Gtk.DrawingArea
+      [ #widthRequest := pageWidth
+      , #heightRequest := pageHeight
+      , On #draw $ handleDraw surfaceRef ?self
+      , On #buttonPressEvent $ \event ->
+          buttonPress event isDrawingRef
+      , On #buttonReleaseEvent $ \event ->
+          buttonRelease event isDrawingRef lastPosRef
+      , On #motionNotifyEvent $ \event ->
+          motionNotify event surfaceRef lastPosRef isDrawingRef penColorRef ?self
+      , On #buttonPressEvent $ \event ->
+          rightClickNotify event menu
+      , On #realize $ realize surfaceRef
+      ]
+  _ <- case addOrInsert of
+    Append -> Gtk.notebookAppendPage notebook drawingArea (Just pageLabel)
+    InsertAfter -> Gtk.notebookInsertPage notebook drawingArea (Just pageLabel) (currentPosition + 1)
+    InsertBefore -> Gtk.notebookInsertPage notebook drawingArea (Just pageLabel) currentPosition
+  resetNotebookLabels notebook
+  #addEvents
+    drawingArea
+    [ Gdk.EventMaskButtonPressMask
+    , Gdk.EventMaskPointerMotionMask
+    , Gdk.EventMaskButtonReleaseMask
+    ]
+  #showAll notebook
 
 removePage :: Gtk.Notebook -> IO ()
 removePage notebook = do
